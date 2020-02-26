098da6ab7f539945982ab5d0da6dde008ffa4365
RJDdev <rahnidemeis@gmail.com>
2020-02-26 14:23:58 +0100

    Initial commit

---
+++ app/src/main/java/app/bluecoral/uidesign_shiftorganiser/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.bluecoral.uidesign_shiftorganiser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
